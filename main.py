9e07cbd7750697fa26a3381f9f466756cc6288c4
lukeeeeeee335 <lukeeeeeee334@gmail.com>
2026-02-04 20:29:18 +0000

    Python Console

--- main.py
+++ main.py
@@ -5,7 +5,13 @@
 #imports
 import os
 import sys
+import getpass
 from modules import *
+from paramiko import SSHClient
+
+
+
+
 
 
 #variable help menu
@@ -15,16 +21,66 @@ banner = """
 |  |__   \   \/   /  |  |_)  |       |  |_)  |      /  ^  \ `---|  |----`
 |   __|   \_    _/   |   ___/        |      /      /  /_\  \    |  |     
 |  |        |  |     |  |            |  |\  \----./  _____  \   |  |     
-|__|        |__|     | _|            | _| `._____/__/     \__\  |__|     
-                                                                                                                                                                                        
+|__|        |__|     | _|            | _| `._____/__/     \__\  |__|
+
+        [::] Remote access toolkit for Final Year Project [::]
+        [::] Created by : Luke O'Sullivan [::]                                                                                                                                                                                
 """
 
 help_menu = """
+        Arguments:
+            -t <ipaddress>   = Run rat on target ip address
+            XXX.rat = configuration file to add to rat
+
+        Example:
+            python3 main.py -t 192.168.56.100
+            python3 main.py -f luke.rat
+"""
+options_menu = """
+        [*] Select A Number to select a Payload [*]
+
+        Payloads:
+            [0] Remote Console
+            [1] Keylogger
 
 """
-#gets arguments
-def parser():
-    pass
+
+username = getpass.getuser()
+header = f"{username}@rat $"
+
+#Read config file
+def read_config(config_file):
+    configuration = {} #empty dictonary
+    read_lines = open(config_file, "r").readlines() #"r" is open in read mode, function just reads line of the config file 
+    configuration["IPADDRESS"] = read_lines[0].strip()
+    configuration["PASSWORD"] = read_lines[1].strip()
+    configuration["WORKINGDIRECTORY"] = read_lines[2].strip()
+
+    return configuration
+
+def os_detection():
+    #Windows
+    if os.name == "nt":
+        return "w"
+    #Other
+    if os.name == "posix":
+        return "L"
+    
+#connect rat to target
+def connect():
+
+    configuration = read_config(sys.argv[1])
+
+    #get config info
+    ipv4 = configuration.get("IPADDRESS")
+    target_password = configuration.get("PASSWORD")
+    working_dir = configuration.get("WORKINGDIRECTORY")
+
+    #REMOTLY CORRECT
+    os.system(f"sshpass -p \"{target_password}\" ssh rat@{ipv4}")
+
+    #target = SSHClient()
+    #target.connect(ipv4, username='rat', password=target_password)
 
 #Command Line interface
 def cli(arguments):
@@ -33,11 +89,15 @@ def cli(arguments):
 
     #if arguments exsist
     if arguments:
-        pass
+        print(options_menu)
+        option = input(f"{header}")
 
+        if option == "0":
+            connect()
     # If arguments dont exsist
     else:
         print(help_menu)
+
 
 #main code
 def main():
@@ -53,5 +113,5 @@ def main():
     cli(arguments_exist)
 
 #runs main code
-if __name__ =='__main__':
+if __name__ =="__main__":
     main()
